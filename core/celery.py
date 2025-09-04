f7e69af39f4a1a93f1ec7878769d47ca1e36b325
amiraliav6 <amiralivahidi6666@gmail.com>
2025-09-04 17:40:34 +0330

    add comment on celery file

--- core/celery.py
+++ core/celery.py
@@ -3,7 +3,7 @@ import os
 from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
-
+# this is the celery config
 app = Celery("core")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
